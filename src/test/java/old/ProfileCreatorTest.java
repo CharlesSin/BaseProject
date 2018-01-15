3c2d3cfa1e52286614500e28267e5ab480fc9e0b
Charles Sin <charlessin1993@gmail.com>
2018-01-15 21:21:06 +0800

    Test Case For Analysis Data & Test Case For Recode And Decode

--- src/test/java/old/ProfileCreatorTest.java
+++ src/test/java/old/ProfileCreatorTest.java
@@ -1,25 +1,32 @@
 package old;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.Assert;
+//import org.apache.commons.io.FileUtils;
+//import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
+//import java.io.File;
 import java.io.IOException;
+//import java.nio.file.Files;
 
 import static org.junit.Assert.*;
 
 public class ProfileCreatorTest {
-    @Test
-    public void testFile() throws IOException {
-        int score = 87;
-        int conduct = 78;
-        hobbyType hobby = hobbyType.Movies;
-        iSubject subject = iSubject.literature;
-        ProfileCreator.create(score, conduct, hobby, subject);
-        final File expected = new File("TestProfile.txt");
-        final File output = new File("Profile.txt");
+  @Test
+  public void testFile() throws IOException {
+    int score = 87;
+    int conduct = 78;
+    hobbyType hobby = hobbyType.Movies;
+    iSubject subject = iSubject.literature;
 
-        Assert.assertEquals(FileUtils.readLines(expected),FileUtils.readLines(output));
-    }
+    String real = "All Pass";
+    String ans = ProfileCreator.create(score, conduct, hobby, subject);
+    // File expected = new File("TestProfile.txt");
+    // File output = new File("Profile.txt");
+
+    String strReal = String.valueOf(real);
+    String strAns = String.valueOf(ans);
+    assertEquals(strReal, strAns);
+
+    // Assert.assertEquals(FileUtils.readLines(expected), Files.lines(output));
+  }
 }
